898872ce53110a4b6340d371b70be09c95281b9f
SungEun <sungeun97@gmail.com>
2021-04-30 02:55:19 +0900

    버튼 눌러서 검색하기

--- client/src/components/views/LandingPage/Sections/SearchFeature.js
+++ client/src/components/views/LandingPage/Sections/SearchFeature.js
@@ -1,22 +1,42 @@
 import React, { useState } from 'react'
-import { Input } from 'antd';
+import { Select, Input } from 'antd';
 
 const { Search } = Input;
+const { Option } = Select;
 
 function SearchFeature(props) {
     const [SearchTerm, setSearchTerm] = useState("")
+    const [SearchCategory, setSearchCategory] = useState("total")
 
-    const onChangeSearch = (event) => {
-        setSearchTerm(event.currentTarget.value)
-
-        props.refreshFunction(event.currentTarget.value)
+    const onSearch = (value) => {
+        setSearchTerm(value)
+        console.log(`Search Category ${SearchCategory}`);
+        props.refreshFunction(value)
+    }
+    const onSearchSelectChange = (value) => {
+        //console.log(`selected ${value}`);
+        setSearchCategory(value)
     }
 
     return (
         <div>
+         
+            <Select
+                defaultValue="total"
+                style={{ width: 100 }}
+                onChange={onSearchSelectChange}
+            >
+                <Option value="total">전체</Option>
+                <Option value="title">제목</Option>
+                <Option value="designer">작가</Option>
+                <Option value="writer">작성자</Option>
+
+            </Select>
+
             <Search
-                value={SearchTerm}
-                onChange={onChangeSearch}
+                allowClear
+                enterButton
+                onSearch={onSearch}
                 placeholder="Search By Typing..."
                 style={{ width: 200 }}
             />
