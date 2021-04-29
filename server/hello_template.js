69f63d50a50b4b499a55f403c54cd0633b08845b
SungEun <sungeun97@gmail.com>
2021-04-30 01:03:33 +0900

    register page 오류 수정

--- server/hello_template.js
+++ server/hello_template.js
@@ -6,10 +6,13 @@ const Hello = data => {
             <head>
                 <title>이메일 인증하기</title>
             </head>
-            <body style="margin: 0; padding: 0;">
+            <body style="margin: 0; padding: 0; font-size:15px;">
                 <br />
                 <br />
-                <div>인증번호는 ${data} 입니다.</div>
+                <div>안녕하세요?</div>
+                <div>디지털 문방구입니다.</div>
+                <div>인증번호를 입력하여 회원가입을 완료하실 수 있습니다.</div>
+                <div style="background-color: skyblue; width:26%; font-size:20px;">인증번호는 <strong style="font-size:25px;">${data}</strong> 입니다.</div>
                 <br />
                 <br />
             </body>
