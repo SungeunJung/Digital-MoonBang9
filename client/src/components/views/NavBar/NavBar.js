b63393b3b22edc0cdc882a8c651362196ef4367f
YuJeong <nanewzeon@gmail.com>
2021-05-29 23:41:43 +0900

    반응형 Navbar 수정

--- client/src/components/views/NavBar/NavBar.js
+++ client/src/components/views/NavBar/NavBar.js
@@ -1,13 +1,4 @@
 import React, { useState } from 'react';
-<<<<<<< HEAD
-import LeftMenu from './Sections/LeftMenu';
-import RightMenu from './Sections/RightMenu';
-import { Drawer, Button, Space} from 'antd';
-//import Icon from '@ant-design/icons';
-import './Sections/Navbar.css';
-
-function NavBar() {
-=======
 import Menubar from './Sections/Menubar';
 import { Drawer, Button, Space} from 'antd';
 //import Icon from '@ant-design/icons';
@@ -15,7 +6,6 @@ import './Sections/Navbar.css';
 import Buttons from './Sections/Buttons';
 
 function NavBar(props) {
->>>>>>> hwang
   const [visible, setVisible] = useState(false)
 
   const showDrawer = () => {
@@ -27,36 +17,6 @@ function NavBar(props) {
   };
 
   return (
-<<<<<<< HEAD
-    <nav className="menu" style={{ position: 'fixed', zIndex: 5, width: '100%' }}>
-      <div className="menu__container">
-        <div className="menu_left">
-          <LeftMenu mode="horizontal" />
-        </div>
-        <div className="menu_right">
-          <RightMenu mode="horizontal" />
-        </div>
-        <Button
-          className="menu__mobile-button"
-          type="primary"
-          onClick={showDrawer}
-        >
-          
-        </Button>
-        <Drawer
-          title="Basic Drawer"
-          placement="right"
-          className="menu_drawer"
-          closable={false}
-          onClose={onClose}
-          visible={visible}
-        >
-          <LeftMenu mode="inline" />
-          <RightMenu mode="inline" />
-        </Drawer>
-      </div>
-    </nav>
-=======
     <div style={{ position: 'fixed', zIndex: 5, width: '100%', height:'18%'}}>
       <div className="logo_bg">
         <a className="logo" href="/">디지털 문방구</a>
@@ -70,7 +30,6 @@ function NavBar(props) {
         </div>
       </nav>
     </div>
->>>>>>> hwang
   )
 }
 
