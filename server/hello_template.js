6320149aad80a1fed1da332be92147fc761b71fa
SungEun <sungeun97@gmail.com>
2021-06-02 13:49:19 +0900

    이메일 템플릿 수정

--- server/hello_template.js
+++ server/hello_template.js
@@ -10,12 +10,10 @@ const Hello = data => {
                 <br />
                 <br />
                 <div>인증번호는 ${data} 입니다. 안녕하세요</div>
-=======
                 <div>안녕하세요?</div>
                 <div>디지털 문방구입니다.</div>
                 <div>인증번호를 입력하여 회원가입을 완료하실 수 있습니다.</div>
                 <div style="background-color: skyblue; width:26%; font-size:20px;">인증번호는 <strong style="font-size:25px;">${data}</strong> 입니다.</div>
->>>>>>> master
                 <br />
                 <br />
             </body>
